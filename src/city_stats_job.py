a29744aa3b9bb499d87c119939643f802f404fd9
Goopard <nick.amirkhanov@gmail.com>
2018-06-29 21:21:48 +0300

    HW3#2

--- src/city_stats_job.py
+++ src/city_stats_job.py
@@ -5,6 +5,9 @@ from user_agents import parse
 
 
 class CityStatsJob(MRJob):
+    """
+    This job calculates the amount of high-bid priced (with price more than value of the argument --min_price) per city.
+    """
     INTERNAL_PROTOCOL = PickleProtocol
 
     KEY_FIELD_SEPARATOR = ';'
@@ -12,9 +15,19 @@ class CityStatsJob(MRJob):
     REGEXP = re.compile('([a-z\d]+)\s+(\d+)\s+(\d+)\s+([\w\d~]+)\s+(?P<user_agent>.*)\s+([\d]+\.[\d]+\.[\d]+\.\*)\s+([\w\d]+)\s+(?P<city_id>[\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+([\w\d]+)\s+(?P<price>[\w\d]+)')
 
     def partitioner(self):
+        """
+        Partitioner which is used to distribute the values between the reducers.
+        """
         return 'org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner'
 
     def configure_options(self):
+        """
+        Configuring options:
+            --cities - file with the relations between the names of the cities and their ids
+            --partitioner - whether to use the partitioner or not
+            --min_price - minimal price to count as "high-bid"
+            --reduces - number of reducers to use
+        """
         super().configure_options()
         self.add_file_option("--cities")
         self.add_passthrough_option("--partitioner", action='store_true')
@@ -22,6 +35,9 @@ class CityStatsJob(MRJob):
         self.add_passthrough_option("--reduces", type='int', default=1)
 
     def jobconf(self):
+        """
+        Configuring the JOBCONF in case the --partitioner is enabled.
+        """
         conf = super().jobconf()
         if self.options.partitioner:
             enable_partitioner = {'mapreduce.job.reduces': self.options.reduces,
@@ -31,9 +47,16 @@ class CityStatsJob(MRJob):
         return conf
 
     def mapper_init(self):
+        """
+        This function is used to initialize the cache dict self.known_user_agents.
+        """
         self.known_user_agents = {}
 
     def mapper(self, _, line):
+        """
+        This is the mapper function of the job, which parses the given line using self.REGEXP and extracts the city id,
+        price and user agent from this line.
+        """
         try:
             values_dict = re.match(self.REGEXP, line).groupdict()
             city_id = values_dict['city_id']
@@ -59,9 +82,16 @@ class CityStatsJob(MRJob):
             pass
 
     def combiner(self, key, values):
+        """
+        This the combiner function, it just combines some values.
+        """
         yield key, sum(values)
 
     def reducer(self, key, values):
+        """
+        This is the reducer function, which finds out the cities name for the city id, sums the values and yields the
+        resulting pair.
+        """
         if self.options.partitioner:
             city_id, user_agent = key.split(';')
         else:
